260e3ad960cc4a7ff367ed5f9d42095183f07476
Joseph Robinson <smartech56@gmail.com>
2019-02-02 14:35:44 -0700

    Aded a command that asks the user which java they would like to use in their ubuntu install.

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,3 +1,4 @@
 echo "Starting"
 sudo apt update
 sudo apt install aptitude chromium-browser maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
+sudo update-alternatives --config java
