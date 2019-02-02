a3442755ffd7ba3b60c078bceae887c096d9ccb2
Joseph Robinson <smartech56@gmail.com>
2019-02-02 15:06:27 -0700

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 echo "Starting"
 sudo apt update
-sudo apt install aptitude chromium-browser maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
+sudo apt install aptitude chromium-browser nautilus-admin maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
 sudo update-alternatives --config java
 
 echo "Installing Jenkins"
