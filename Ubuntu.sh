0897da1a5aa01522d77a43b8af4d4e6a22e7e33e
Joseph Robinson <smartech56@gmail.com>
2019-02-02 15:01:38 -0700

    Added Jenkins Install

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -2,3 +2,10 @@ echo "Starting"
 sudo apt update
 sudo apt install aptitude chromium-browser maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
 sudo update-alternatives --config java
+
+echo "Installing Jenkins"
+echo "" | sudo tee -a /etc/apt/sources.list
+echo "# Jenkins Apt Repository" | sudo tee -a /etc/apt/sources.list
+echo "deb https://pkg.jenkins.io/debian binary/" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install jenkins -y
