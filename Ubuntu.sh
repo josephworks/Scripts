db553e11f8a0e0ec69e4333b8df65e4a03dfeece
Joseph Robinson <smartech56@gmail.com>
2019-02-18 19:23:06 -0700

    Replaced Screenfetch with NeoFetch

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 echo "Starting"
 sudo apt update
-sudo apt install aptitude chromium-browser nautilus-admin maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
+sudo apt install aptitude chromium-browser nautilus-admin maven gradle git wget tree toilet figlet curl apache2 mysql-client mysql-server php visualvm openjdk-8-jre-headless htop screenfetch neofetch nodejs npm fish openssh-server vsftpd filezilla python python2 -y
 sudo update-alternatives --config java
 
 read -p "Would you like to install Jenkins? " -n 1 -r
